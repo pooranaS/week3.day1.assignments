2e731ad8e058485abc607ec4e4a484cdf546dafe
pooranaS <spooranakala@gmail.com>
2022-05-05 20:59:14 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,11 @@
+package org.department;
+
+import week3.day1.assignments.org.college.College;
+
+public class Department extends College{
+
+	public void deptName()
+	{
+		System.out.println("the dept name is EIE");
+	}
+}
--- Student.java
+++ Student.java
@@ -1,10 +1,34 @@
-package week3.day1.assignments.org.college;
+package org.student;
 
-public class Student {
+import org.department.Department;
 
+public class Student extends Department {
+
+	public void studentName()
+	{
+		System.out.println("The student name is Poorna");
+	}
+	public void studentDept()
+	{
+		System.out.println("The student dept is EIE");
+	}
+	public void studentId()
+	{
+		System.out.println("the student id is 103");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Student s = new Student();
+		System.out.println("Output of college class");
+		s.collegeName();
+		s.collegeCode();
+		s.collegeRank();
+		System.out.println("output of department class");
+		s.deptName();
+		System.out.println("output of student class");
+		s.studentName();
+		s.studentDept();
+		s.studentId();
 	}
 
 }
